0ebc84497aea24bcf44cd0e839eac5fdbe38c2b2
VM TD <t.dominiak@valuemedia.pl>
2026-01-13 23:41:24 +0100

    menu - dodanie zaznaczania miesięcy

--- src/app/html-menu/html-menu.ts
+++ src/app/html-menu/html-menu.ts
@@ -12,5 +12,21 @@ yearMonths = Array.from({ length: 12 }, (_, i) => {
   date.setMonth(date.getMonth() - i);
   return date.toISOString().slice(0, 7);
 });
+selectedMonths: string[] = [];
+
+public get monthsSelected(): boolean {
+  return this.selectedMonths.length > 0;
+}
+
+onMonthChange(event: Event, month: string) {
+  const checkbox = event.target as HTMLInputElement; // Rzutowanie na odpowiedni typ
+  const isChecked = checkbox.checked; // Pobranie wartości true/false
+
+  if (isChecked) {
+    this.selectedMonths.push(month);
+  } else {
+    this.selectedMonths = this.selectedMonths.filter(m => m !== month);
+  }
+}
 
 }
